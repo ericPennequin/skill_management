c481a439482fd784ae6a05dfc31b30910e9a535b
Damien <damien@cefimmac08.local>
2018-02-12 11:40:36 +0100

    header and footer moved to inc/

--- inc/header.php
+++ inc/header.php
@@ -8,13 +8,13 @@ if (!isset($pageTitle) || empty($pageTitle))
 <html>
 <head>
     <title><?= $pageTitle ?></title>
-    <link rel="icon" type="image/png" href="favicon.png"/>
+    <link rel="icon" type="image/png" href="../favicon.png"/>
     <!--[if IE]>
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/><![endif]-->
-    <script src="./lib/jquery-3.2.1/jquery-3.2.1.js"></script>
-    <script defer src="./lib/fontawesome-free-5.0.6/js/fontawesome-all.js"></script>
-    <script src="./lib/bootstrap-4.0.0-dist/js/bootstrap.js"></script>
-    <link rel="stylesheet" href="./lib/bootstrap-4.0.0-dist/css/bootstrap.css">
+    <link rel="shortcut icon" type="image/x-icon" href="../favicon.ico"/><![endif]-->
+    <script src="../lib/jquery-3.2.1/jquery-3.2.1.js"></script>
+    <script defer src="../lib/fontawesome-free-5.0.6/js/fontawesome-all.js"></script>
+    <script src="../lib/bootstrap-4.0.0-dist/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="../lib/bootstrap-4.0.0-dist/css/bootstrap.css">
 
     <?php // PAGE PROFIL
     if ($currentPageName === "profil.php") {
