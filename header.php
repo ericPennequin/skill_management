69113ef1729329ed7b459e24b59e37eb8932c5ba
Damien <damien@cefimmac08.local>
2018-02-12 10:40:24 +0100

    include libraries and favicons
    bootstrap, jquery, fontawesome

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= $page_title ?></title>
+    <link rel="icon" type="image/png" href="favicon.png" />
+    <!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="favicon.ico" /><![endif]-->
+    <script src="./lib/jquery-3.2.1/jquery-3.2.1.js"></script>
+    <script defer src="./lib/fontawesome-free-5.0.6/js/fontawesome-all.js"></script>
+    <script src="./lib/bootstrap-4.0.0-dist/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="./lib/bootstrap-4.0.0-dist/css/bootstrap.css">
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: damien
- * Date: 05/02/2018
- * Time: 17:22
- */
+include 'header.php';
+?>
+index.
+<?php
+include 'footer.php';
+?>
