926fbc7a2345fdbb59f232ad807b8514caee1af5
Damien <damien@cefimmac08.local>
2018-02-13 14:43:08 +0100

    avancements

--- inc/functions.php
+++ inc/functions.php
@@ -72,3 +72,84 @@ function displayPhoneNumber($number, $format = "normal")
             return $number;
     }
 }
+
+/**
+ * Affiche la liste des compétences passées dans un tableau
+ * @param array $skills la liste des compétences
+ */
+function displaySkills(array $skills)
+{
+    if (count($skills) > 0) {
+        foreach ($skills as $id => $skill) {
+            echo "<a class=\"ct competence-tag\" data-competence-title=\"${skill}\" id=\"ct-${id}\" href=\"#${skill}-${id}\">${skill}</a>";
+        }
+    } else {
+        echo "Il n'y a aucune compétence à afficher.";
+    }
+}
+
+/**
+ * Affiche la liste des pièces jointes passées dans un tableau
+ * @param array $attachments la liste des pièces jointes
+ */
+function displayAttachments(array $attachments)
+{
+
+}
+
+/**
+ * Affiche la description d'un projet
+ * @param string $description la description du projet
+ * @param int $id l'identifiant du projet
+ */
+function displayDescription($description, $id)
+{
+    if (strlen($description) > 340) {
+        $maxChars = 350;
+        $strStart = substr($description, 0, $maxChars);
+        $strEnd = substr($description, $maxChars);
+        ?>
+        <span class="description-start description-hidden" data-description-start="<?= $id ?>"><?= $strStart ?></span>
+        <a class="show-more" data-description-id="<?= $id ?>" href="#pp-<?= $id ?>">En savoir plus</a>
+        <span class="description-end" data-description-end="<?= $id ?>"><?= $strEnd ?></span>
+    <?php } else {
+        echo $description;
+    }
+
+}
+
+/**
+ * Affiche la liste des projets passées dans un tableau
+ * @param array $projects la liste des projets
+ */
+function displayProjects(array $projects)
+{
+    if (count($projects) > 0) {
+        foreach ($projects as $id => $project) { ?>
+            <div class="profil-projet"
+                 data-projet-title="<?= $project["name"] ?>"
+                 id="pp-<?= $project["id_project"] ?>">
+                <h3 class="projet-header"><?= $project["name"] ?></h3>
+                <div class="projet-location">LJK<?= $project["id_establishment"] ?>JYGHB</div>
+                <p class="projet-description">
+                    <? displayDescription($project["description"], $project["id_project"]) ?>
+                </p>
+                <div class="projet-competences">
+                    <?php displaySkills($project["skills"]) ?>
+                </div>
+                <div class="projet-files">
+                    <a class="pf pf-word" href="" download="download">test_file.docx</a>
+                    <a class="pf pf-pdf" href="" download="download">test_file.pdf</a>
+                    <a class="pf pf-img" href="" download="download">test_file.png</a>
+                    <a class="pf pf-default" href="" download="download">test_file.bin</a>
+                </div>
+                <?php if (count($project["attachments"]) > 0) {
+                    displayAttachments($project["attachments"]);
+                } ?>
+            </div>
+            <?php
+        }
+    } else {
+        echo "Il n'y a aucun projet à afficher.";
+    }
+}
--- js/profil.js
+++ js/profil.js
@@ -13,4 +13,18 @@ $(function() {
         section.toggleClass("ps-open ps-closed");
         $("#sc-"+sectionName).slideToggle("slide");
     }
+
+    // Description
+    var showMore = $(".show-more");
+
+    showMore.on('click', toggleDescription)
+
+    function toggleDescription() {
+        var id = $(this).attr('data-description-id');
+        var strStart = $("[data-description-start='"+id+"']");
+        var strEnd = $("[data-description-end='"+id+"']");
+        strStart.removeClass("description-hidden");
+        strStart.append(strEnd.text());
+        $(this).toggle();
+    }
 });
