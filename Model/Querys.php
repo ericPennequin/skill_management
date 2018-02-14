91a9e118ce252e4f639e85caedc19ab0939b544d
Eric Pennequin <eric.pennequin@e5group.fr>
2018-02-14 12:12:01 +0100

    lib datatable + querys

--- Model/Querys.php
+++ Model/Querys.php
@@ -15,43 +15,33 @@ $data=$_REQUEST;
 $data['command']=$_REQUEST['command'];
 
 
-/*
-//recherche
-if ($data['command']=='search'){
-	$wordSearch='%'.$data['search'].'%';
-
-	$sql="SELECT * FROM person as p, establishment as e, skills as s  WHERE (p.firstname OR p.lastname) LIKE $wordSearch OR  ";// il faut le resultat de la recherche
-	if ($data['localisation'] !=""){
-		$filterLocalisation=$data['localisation'];
-		$sql .="AND city = $filterLocalisation";//de la table establishment
-	}
-	if ($data['techno'] !=""){
-		$filterTechno=$data['techno'];
-		$sql .="AND name = $filterTechno";//de la table skills
-	}
-
-}
-*/
-
-
 
 /**Connexion user
  * Return table of 2 objects
  * TESTE OK
 */
 if ($data['command']=='userConnection'){
-
+	$userConnectionWords=[];
 		$result=[];
 
 		try {
-			$userConnectionWords= $dbh->exec("SELECT search, counter FROM search ORDER BY counter DESC LIMIT 10");
+
+			$request= $dbh->query("SELECT search, counter FROM search ORDER BY counter DESC LIMIT 10");
+
+			$userConnectionWords=$request->fetchAll();
+
 			$result['userConnectionWords']=$userConnectionWords;
-			$userConnectionLastIncomming= $dbh->exec("SELECT * FROM person 
+
+
+			$request= $dbh->query("SELECT * FROM person 
 					WHERE admin = 0 AND status = 1 AND firstname is not null AND lastname is not null 
 					ORDER BY id_person DESC LIMIT 3");
+			$userConnectionLastIncomming=$request->fetchAll();
 			$result['userConnectionLastIncomming']=$userConnectionLastIncomming;
 
-			return $result;
+			$resultJson=json_encode($result);
+
+			echo $resultJson;
 
 		}catch(Exception $e){
 			die('Erreur: '.$e->getMessage());
@@ -65,11 +55,32 @@ if ($data['command']=='userConnection'){
 
 
 /** Consultation d'un profil
- * Return Result [userData][projectData][filesData][competencys[index]]
+ * Return Result :
+ * --------------------------------------------
+ *  userDate [ARRAY]
+ *      firstname
+ *      lastname
+ *      email
+ *      cell_number
+ *      id_establishment
+ *      name (establishment)
+ *      city (establishment)
+ *      project [ARRAY, id = id_project]
+ *          id_project
+ *          name
+ *          description
+ *          status
+ *          data [ARRAY]
+ *              id_data
+ *              data
+ *          skills [ARRAY]
+ *              id_skill
+ *              name
+ * --------------------------------------------
  * Tests unitaires : OK
  */
 
-if ($data['command']=='profilViewer'){
+if ($data['command'] == 'profilViewer') {
 
 	/**
 	 * PArtie profil :
@@ -80,38 +91,61 @@ if ($data['command']=='profilViewer'){
 	 * Compétences associées (table skill et link_project_skill)
 	 *
 	 */
-	$id_project=$data['id_project'];
-	$id_Establishment=$data['id_Establishment'];
-	$is_person=$data['id_person'];
+	$id_person = $data['id_person'];
 
 	try {
-
-		$userData= $dbh->exec("SELECT firstname,lastname,email,cell_number, establishment.name, establishment.city 
+		// Récupération du profil et de l'établissement
+		$stmt = $dbh->prepare("SELECT firstname,lastname,email,cell_number, establishment.id_establishment, establishment.name, establishment.city 
 			FROM person
 			JOIN establishment ON person.id_establishment=establishment.id_establishment 
-			WHERE id_person='$is_person' ");
+			WHERE id_person=:id_person");
+		$stmt->execute(array("id_person" => $id_person));
 
-		$result['userData']=$userData;
+		$result['userData'] = $stmt->fetch();
 
-		$projectData= $dbh->exec("SELECT name, description FROM project WHERE id_project='$id_project' ");
-		$result['projectData']=$projectData;
+		// Récupération des projets
+		$stmt = $dbh->prepare("SELECT * FROM project WHERE id_establishment=:id_establishment");
+		$stmt->execute(array("id_establishment" => $result['userData']['id_establishment']));
 
-		$filesData= $dbh->exec("SELECT data FROM data WHERE id_project='$id_project' ");
-		$result['filesData']=$filesData;
+		while ($row = $stmt->fetch()) {
+			$db_project_id = $row['id_project'];
+			$db_project_infos = array(
+				"id_project" => $db_project_id,
+				"name" => $row["name"],
+				"description" => $row["description"],
+				"status" => $row["status"]
+			);
+			$result['userData']['projects'][$db_project_id] = $db_project_infos;
+			unset($db_project_id);
+			unset($db_project_infos);
+		}
+		unset($row);
 
-		$competencysArray=$dbh->exec("SELECT id_skill FROM link_project_skill WHERE id_project='$id_project' ");
+		// Récupération des pièces jointes et des compétences
+		foreach ($result['userData']['projects'] as $id => $project) {
 
-		for ($idx=0; $idx < count($competencysArray); $idx++){
-			$idCompetency=$competencysArray[$idx];
-			$competency=$dbh->exec("SELECT name FROM skill WHERE id_skill='$competencysArray[$idx]' ");
-			$result['competencys'][$idx]=$competency;
+			// Pièces jointes
+			$stmt = $dbh->prepare("SELECT id_data, data FROM data WHERE id_project=:id_project");
+			$stmt->execute(array("id_project" => $id));
 
+			$result['userData']['projects'][$id]['data'] = $stmt->fetchAll();
+
+			// Compétences
+			$stmt = $dbh->prepare("SELECT skill.id_skill, name
+			FROM skill
+			JOIN link_project_skill ON skill.id_skill=link_project_skill.id_skill 
+			WHERE id_project=:id_project");
+			$stmt->execute(array("id_project" => $id));
+
+			$result['userData']['projects'][$id]['skills'] = $stmt->fetchAll();
+
+			unset($id);
+			unset($project);
 		}
 
-
 		return $result;
-	}catch(Exception $e){
-		die('Erreur: '.$e->getMessage());
+	} catch (Exception $e) {
+		die('Erreur: ' . $e->getMessage());
 	}
 
 	/*Pour test SQL
@@ -126,8 +160,6 @@ if ($data['command']=='profilViewer'){
 	 * */
 
 }
-
-
 
 
 
@@ -152,8 +184,10 @@ if ($data['command']=='profilModif'){
 	try {
 		if ($id_Establishment == ""){
 
-			$establishmentUpdate=$dbh->exec("INSERT INTO establishment(name,city)VALUES('$establishmentName','$city') ");
+			$request=$dbh->query("INSERT INTO establishment(name,city)VALUES('$establishmentName','$city') ");
 		}
+		$establishmentUpdate=$request->fetchAll();
+
 		//TODO Voir si c'est nécessaire (en cas de 2 requêtes pour récupérer l'id etablissement)
 		//$id_Establishment=$dbh->exec("SELECT establishment.id_establishment FROM establishment WHERE establishment.name = 'CEFIM' AND establishment.city='Tours' ");
 
@@ -185,7 +219,7 @@ if ($data['command']=='profilModif'){
 
 		 * */
 
-		$userProfilUpdate=$dbh->exec("UPDATE person
+		$request=$dbh->query("UPDATE person
 		set
 		firstname='$firsname',
 		lastname='$lastname',
@@ -196,6 +230,8 @@ if ($data['command']=='profilModif'){
 			WHERE establishment.name = '$establishmentName' 
 			AND establishment.city='$city')
 		 WHERE id_person = '$userId' ");
+
+		$userProfilUpdate=$request->fetchAll();
 
 
 	}catch(Exception $e){
@@ -228,9 +264,14 @@ if ($data['command']=='projectAdd'){
 
 	try {
 
-		$projectCreate=$dbh->exec("INSERT INTO project (id_establishment,name,description,	status) 
+		$request=$dbh->query("INSERT INTO project (id_establishment,name,description,	status) 
 			VALUES('$id_Establishment','$projectName','$projectDescription',1 ) ");
-		$projectSelectId=$dbh->exec("SELECT id_project FROM project ORDER BY id_project DESC LIMIT 1 ");
+
+		$projectCreate=$request->fetchAll();
+
+		$request=$dbh->query("SELECT id_project FROM project ORDER BY id_project DESC LIMIT 1 ");
+
+		$projectSelectId=$request->fetchAll();
 
 		for ($idx=0; $idx<count($competencys);$idx ++){
 			$currentCompetency=$competencys[$idx];
@@ -275,7 +316,7 @@ if ($data['command']=='projectModif'){
 
 	try {
 
-		$projectCreate=$dbh->exec(" UPDATE project set id_establishment='$id_Establishment', 
+		$projectCreate=$dbh->query(" UPDATE project set id_establishment='$id_Establishment', 
  				name='$projectName', description='$projectDescription',status='$projectStatute' WHERE id_project='$id_project' ");
 
 		/*POUR TESTS
@@ -312,7 +353,7 @@ if ($data['command']=='addUser'){
 	$pwd=$data['pwd'];
 
 	try {
-		$addUser= $dbh->exec("INSERT INTO person (email,pwd,id_establishment,admin,status)
+		$addUser= $dbh->query("INSERT INTO person (email,pwd,id_establishment,admin,status)
 			VALUES ($email,$pwd,1,0,1)");
 
 	}catch(Exception $e){
@@ -337,7 +378,7 @@ if ($data['command']=='addUser'){
 if ($data['command']=='deleteUser'){
 	$id=$data['id'];
 	try {
-		$deleteUser= $dbh->exec("UPDATE person set status=0 WHERE id_person = '$id'");
+		$deleteUser= $dbh->query("UPDATE person set status=0 WHERE id_person = '$id'");
 
 	}catch(Exception $e){
 		die('Erreur: '.$e->getMessage());
